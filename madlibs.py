41008cc5d1ae2d6e5a22d36e381c9061ae8caeac
Leonardo Fragoso <110063706+LeonardoRFragoso@users.noreply.github.com>
2023-02-11 02:41:31 -0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjetivo: ")
+verb1 = input("Verbo: ")
+verb2 = input("Verbo: ")
+famous_person = input("Pessoa Famosa: ")
+
+madlib = f"A programação de computadores é tão {adj}! Isso me deixa tão animado o tempo todo porque \
+eu amo {verb1}. Mantenha-se hidratado e {verb2} como se você fosse {famous_person}!"
+
+print(madlib)
